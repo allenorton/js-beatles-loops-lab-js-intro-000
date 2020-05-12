5d4494b2ea9dd4815378be187aa63ced9054b5d1
Elizabeth Allene Norton <norton.allene@gmail.com>
2020-05-12 17:09:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var whoPlays = []
   for (var x = 0; x < musicians.length; x ++){
-    whoPlays.push(musicians[x] + "plays" +instruments[x])
+    whoPlays.push(musicians[x] + " plays " +instruments[x])
   }
   return whoPlays
 }
